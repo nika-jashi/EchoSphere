1e6cdb4418b78622b5d639cc517b66825bae1d89
nika jashi <jashi.nika1@gmail.com>
2024-01-04 18:06:05 +0400

    added profile for users but it is not accessable through site for now

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -1,3 +1,7 @@
+import os
+import uuid
+from enum import Enum
+
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 from django.contrib.auth.models import (
@@ -54,3 +58,35 @@ class CustomAccount(AbstractUser):
 
     def __str__(self):
         return self.username
+
+
+def profile_picture_file_path(instance, filename):
+    """ Generate file path for new recipe image """
+    ext = os.path.splitext(filename)[1]
+    filename = f'{uuid.uuid4()}{ext}'
+
+    return os.path.join('uploads', 'users', filename)
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(CustomAccount, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=32, null=True)
+    last_name = models.CharField(max_length=32, null=True)
+    date_of_birth = models.DateField(null=True)
+    bio = models.TextField(blank=True)
+    profile_picture = models.ImageField(upload_to=profile_picture_file_path, null=True)
+    GENDER_CHOICES = (
+        ('U', _('Unknown')),
+        ('M', _('Male')),
+        ('F', _('Female')),
+        ('O', _('Others')),
+    )
+
+    gender = models.CharField(
+        choices=GENDER_CHOICES,
+        default='U',  # Start with U (Unknown)
+        null=True,
+    )
+
+    def __str__(self):
+        return f"{self.user.username}'s Profile"
