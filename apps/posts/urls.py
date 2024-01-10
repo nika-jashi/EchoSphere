73a44e8f4230935be9a5fe8f6ba0ab8f351dedd9
nika jashi <jashi.nika1@gmail.com>
2024-01-10 18:01:14 +0400

    implemented like/unlike logic works perfectly but user expereance is not good

--- apps/posts/urls.py
+++ apps/posts/urls.py
@@ -1,10 +1,12 @@
 from django.contrib.auth.decorators import login_required as l
 from django.urls import path
 
-from apps.posts.views import CreatePostView, AllPostsView
+from apps.posts.views import CreatePostView, AllPostsView, like_post, unlike_post
 
 app_name = "posts"
 
 urlpatterns = [
     path('create/', l(CreatePostView.as_view(), redirect_field_name='accounts:login'), name='create'),
+    path('like_post/<int:post_id>/', like_post, name='like_post'),
+    path('unlike_post/<int:post_id>/', unlike_post, name='unlike_post'),
 ]
--- apps/posts/views.py
+++ apps/posts/views.py
@@ -1,7 +1,9 @@
+from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.views import View
 
 from apps.posts.forms import PostForm
+from apps.posts.models import Post
 from apps.utils.db_queries import get_all_posts
 
 
@@ -30,3 +32,21 @@ class AllPostsView(View):
         news_feed_posts = get_all_posts()
 
         return render(request, self.template_name, {'news_feed_posts': news_feed_posts})
+
+
+def like_post(request, post_id):
+    if request.user.is_authenticated:
+        post = Post.objects.get(id=post_id)
+        post.likes.add(request.user)
+        post.save()
+        return redirect('news-feed')
+    return HttpResponse(status=400)
+
+
+def unlike_post(request, post_id):
+    if request.user.is_authenticated:
+        post = Post.objects.get(id=post_id)
+        post.likes.remove(request.user)
+        post.save()
+        return redirect('news-feed')
+    return HttpResponse(status=400)
