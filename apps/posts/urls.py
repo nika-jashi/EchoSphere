9f3246d3a4067c6cbee3801552ce686b90010e18
nika jashi <jashi.nika1@gmail.com>
2024-01-16 16:35:45 +0400

    fixed some minor issues and added delete button still plenty work to do with posts

--- apps/posts/urls.py
+++ apps/posts/urls.py
@@ -1,12 +1,20 @@
 from django.contrib.auth.decorators import login_required as l
 from django.urls import path
 
-from apps.posts.views import CreatePostView, AllPostsView, like_post, unlike_post
+from apps.posts.views import (
+    CreatePostView,
+    like_post,
+    unlike_post,
+    DetailPostView,
+    DeletePostView
+)
 
 app_name = "posts"
 
 urlpatterns = [
     path('create/', l(CreatePostView.as_view(), redirect_field_name='accounts:login'), name='create'),
+    path('detail/<int:pk>/', l(DetailPostView.as_view(), redirect_field_name='accounts:login'), name='detail'),
+    path('delete/<int:pk>/', l(DeletePostView.as_view(), redirect_field_name='accounts:login'), name='delete'),
     path('like_post/<int:post_id>/', like_post, name='like_post'),
     path('unlike_post/<int:post_id>/', unlike_post, name='unlike_post'),
 ]
